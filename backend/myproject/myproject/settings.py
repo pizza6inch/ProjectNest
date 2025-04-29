69ee2a279714ca06a0d7657d3385bbfb713910c8
yeyeye0618 <datealife5@gmail.com>
2025-04-29 11:29:39 +0800

    資料庫連線初始設定

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+from decouple import Config, RepositoryEnv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,10 +75,17 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+pymysql.install_as_MySQLdb()
+config = Config(repository=RepositoryEnv("db.env"))
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "projectNest_database",
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": "",
+        "PORT": "",
     }
 }
 

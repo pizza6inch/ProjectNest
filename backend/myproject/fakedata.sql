cefcd788ea19da316e92521254eb5e33ceccd437
yeyeye0618 <datealife5@gmail.com>
2025-05-06 11:02:29 +0800

    假資料部分資料加入

---
+++ backend/myproject/fakedata.sql
@@ -0,0 +1,24 @@
+use projectNest_database;
+INSERT INTO `projectnest_database`.`myapp_user`
+(`user_id`,`name`,`email`,`password`,`role`,`image_url`,`create_at`,`update_at`)
+VALUES
+('1','張曉明','t114590000@ntut.org.tw','123','student','https://google.com','2023-10-01 00:00:00','2023-10-01 00:00:00'),
+('2','李小華','lihsiao@ntut.org.tw','456','professor','https://google.com','2023-10-02 00:00:00','2023-10-02 00:00:00'),
+('3','王大明','wangda@ntut.org.tw','789','student','https://google.com','2023-10-03 00:00:00','2023-10-03 00:00:00'),
+('4','陳美麗','chenmei@ntut.org.tw','101','student','https://google.com','2023-10-04 00:00:00','2023-10-04 00:00:00'),
+('5','林志豪','linzhi@ntut.org.tw','102','professor','https://google.com','2023-10-05 00:00:00','2023-10-05 00:00:00'),
+('6','黃小英','huangxy@ntut.org.tw','103','student','https://google.com','2023-10-06 00:00:00','2023-10-06 00:00:00'),
+('7','周建國','zhoujian@ntut.org.tw','104','student','https://google.com','2023-10-07 00:00:00','2023-10-07 00:00:00'),
+('8','鄭美華','zhengmei@ntut.org.tw','105','student','https://google.com','2023-10-08 00:00:00','2023-10-08 00:00:00'),
+('9','趙子龍','zhaozilong@ntut.org.tw','106','professor','https://google.com','2023-10-09 00:00:00','2023-10-09 00:00:00'),
+('10','孫尚香','sunshangxiang@ntut.org.tw','107','student','https://google.com','2023-10-10 00:00:00','2023-10-10 00:00:00'),
+('11','劉備','liubei@ntut.org.tw','108','student','https://google.com','2023-10-11 00:00:00','2023-10-11 00:00:00'),
+('12','關羽','guanyu@ntut.org.tw','109','professor','https://google.com','2023-10-12 00:00:00','2023-10-12 00:00:00'),
+('13','張飛','zhangfei@ntut.org.tw','110','student','https://google.com','2023-10-13 00:00:00','2023-10-13 00:00:00'),
+('14','諸葛亮','zhugeliang@ntut.org.tw','111','professor','https://google.com','2023-10-14 00:00:00','2023-10-14 00:00:00'),
+('15','黃忠','huangzhong@ntut.org.tw','112','student','https://google.com','2023-10-15 00:00:00','2023-10-15 00:00:00'),
+('16','馬超','machao@ntut.org.tw','113','student','https://google.com','2023-10-16 00:00:00','2023-10-16 00:00:00'),
+('17','龐統','pangtong@ntut.org.tw','114','professor','https://google.com','2023-10-17 00:00:00','2023-10-17 00:00:00'),
+('18','魏延','weiyan@ntut.org.tw','115','student','https://google.com','2023-10-18 00:00:00','2023-10-18 00:00:00'),
+('19','姜維','jiangwei@ntut.org.tw','116','professor','https://google.com','2023-10-19 00:00:00','2023-10-19 00:00:00'),
+('20','廖化','liaohua@ntut.org.tw','117','student','https://google.com','2023-10-20 00:00:00','2023-10-20 00:00:00');

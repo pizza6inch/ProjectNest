44f8cc59575159d1e1ae837034105057f34faff9
泡芙的電腦\user <lbth20021124@gmail.com>
2025-05-08 16:52:16 +0800

    完成user get api

--- backend/myproject/myapp/urls.py
+++ backend/myproject/myapp/urls.py
@@ -3,5 +3,5 @@ from .views import UserListAPIView
 
 urlpatterns = [
     path('api/users', UserListAPIView.as_view({'get': 'users'}), name='user-list'),
-    path('api/users/<str:pk>', UserListAPIView.as_view({'get': 'users_id'}), name='user-list'),
+    path('api/user/<str:pk>', UserListAPIView.as_view({'get': 'user_id'}), name='user-list'),
 ]
--- backend/myproject/myapp/views.py
+++ backend/myproject/myapp/views.py
@@ -1,30 +1,50 @@
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status, viewsets
+from django.core.paginator import Paginator
 from .models import User
 from .serializers import UserSerializer
 
 class UserListAPIView(viewsets.ModelViewSet):
-    @action(detail=True, methods=["get"])
+    @action(detail=False, methods=["get"])
     def users(self, request):
 
-        filter_param = request.query_params.get('filter')
-        sort_by = request.query_params.get('sortBy')
+        role = request.query_params.get('role')
+        sort_by = request.query_params.get('sortBy', 'user_id')
         page = int(request.query_params.get('page', 1))
         page_size = int(request.query_params.get('pageSize', 10))
         
-        # print(filter_param, sort_by, page, page_size)
-
         users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        if role:
+            users = users.filter(role=role)
+        
+        if sort_by:
+            if sort_by in [f.name for f in User._meta.fields]:
+                users = users.order_by(sort_by)
+            else:
+                return Response(
+                {'error': 'Please enter a valid field.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        paginator = Paginator(users, page_size)
+        page_obj = paginator.get_page(page)
+
+        serializer = UserSerializer(page_obj.object_list, many=True)
+        return Response({
+            "total": paginator.count,
+            "page": page,
+            "pageSize": page_size,
+            "results": serializer.data
+        }, status=status.HTTP_200_OK)
     
     @action(detail=True, methods=["get"])
-    def users_id(self, request, pk=None):
-        print(pk)
+    def user_id(self, request, pk=None):
         try:
             user = User.objects.get(user_id=pk)
             serializer = UserSerializer(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except User.DoesNotExist:
             return Response({'error': 'User not found'}, status=404)
+        
+    

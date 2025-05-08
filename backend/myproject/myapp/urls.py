9c066e823de5779e1b9095ae3272be8a24975ef4
泡芙的電腦\user <lbth20021124@gmail.com>
2025-05-08 23:30:29 +0800

    修改path名稱 ，完成create_user api

--- backend/myproject/myapp/urls.py
+++ backend/myproject/myapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import UserListAPIView
 
 urlpatterns = [
-    path('api/users', UserListAPIView.as_view({'get': 'users'}), name='user-list'),
-    path('api/user/<str:pk>', UserListAPIView.as_view({'get': 'user_id'}), name='user-list'),
+    path('api/get_users', UserListAPIView.as_view({'get': 'get_users'}), name='user-list'),
+    path('api/get_user_by_id/<str:pk>', UserListAPIView.as_view({'get': 'get_user_by_id'}), name='user-list'),
+    path('api/create_user', UserListAPIView.as_view({'post': 'create_user'}), name='user-list'),
+    # path('api/update_user/<str:pk>', UserListAPIView.as_view({'put': 'update_user'}), name='user-list'),
+    # path('api/delete_user/<str:pk>', UserListAPIView.as_view({'delete': 'delete_user'}), name='user-list'),
 ]
--- backend/myproject/myapp/views.py
+++ backend/myproject/myapp/views.py
@@ -6,8 +6,9 @@ from .models import User
 from .serializers import UserSerializer
 
 class UserListAPIView(viewsets.ModelViewSet):
+    # 查詢所有使用者
     @action(detail=False, methods=["get"])
-    def users(self, request):
+    def get_users(self, request):
 
         role = request.query_params.get('role')
         sort_by = request.query_params.get('sortBy', 'user_id')
@@ -38,13 +39,45 @@ class UserListAPIView(viewsets.ModelViewSet):
             "results": serializer.data
         }, status=status.HTTP_200_OK)
     
+    # 查詢單一使用者
     @action(detail=True, methods=["get"])
-    def user_id(self, request, pk=None):
+    def get_user_by_id(self, request, pk=None):
         try:
             user = User.objects.get(user_id=pk)
             serializer = UserSerializer(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except User.DoesNotExist:
             return Response({'error': 'User not found'}, status=404)
-        
     
+    # 新增使用者
+    @action(detail=False, methods=["post"])
+    def create_user(self, request):
+        serializer = UserSerializer(data=request.data)
+        print(request.data,serializer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    # # 更新使用者
+    # @action(detail=True, methods=["put"])
+    # def update_user(self, request, pk=None):
+    #     try:
+    #         user = User.objects.get(user_id=pk)
+    #         serializer = UserSerializer(user, data=request.data)
+    #         if serializer.is_valid():
+    #             serializer.save()
+    #             return Response(serializer.data, status=status.HTTP_200_OK)
+    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #     except User.DoesNotExist:
+    #         return Response({'error': 'User not found'}, status=404)
+        
+    # # 刪除使用者
+    # @action(detail=True, methods=["delete"])
+    # def delete_user(self, request, pk=None):
+    #     try:
+    #         user = User.objects.get(user_id=pk)
+    #         user.delete()
+    #         return Response(status=status.HTTP_204_NO_CONTENT)
+    #     except User.DoesNotExist:
+    #         return Response({'error': 'User not found'}, status=404)

13e0b37e19d1d60c764c76ec67d48ead6f8e30dc
pizza6inch <126549982+pizza6inch@users.noreply.github.com>
2025-06-02 08:36:45 +0000

    allow all cors

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -140,8 +140,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://*.vercel.app",
-    "https://*.github.dev"
-]
+CORS_ALLOW_ALL_ORIGINS = True

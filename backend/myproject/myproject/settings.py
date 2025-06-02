18b67814fbe48c948cba4bb505db3f4ba9de603a
pizza6inch <126549982+pizza6inch@users.noreply.github.com>
2025-06-02 08:15:31 +0000

    cors settings

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -142,5 +142,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "project-nest-nu.vercel.app"
+    "https://project-nest-nu.vercel.app/"
 ]
